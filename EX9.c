8c9103ee4670474bd70c349faf19ceb85f47c3d5
HenriqOliveira <64242165+HenriqOliveira@users.noreply.github.com>
2020-04-24 01:14:17 -0300

    Create EX9.c

---
+++ EX9.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+#include <stdlib.h>
+int main()
+{      
+  printf ("#######################\n#                     #\n#                     #\n#    SEJA BEM-VINDO   #\n#                     #\n#                     #\n#######################\n");
+  return 0;  
+}
